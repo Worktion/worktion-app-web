6bbe86e36db4226ea239494e08890e80e9fe8be4
AlanGlezH <47785262+AlanGlezH@users.noreply.github.com>
2020-11-09 10:04:09 -0600

    File loader added

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
+import logo from "../../images/loginImage.png";
+
 function LoginPage() {
   return (
-    <div>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "fixed",
+        backgroundImage: `url(${logo})`,
+      }}
+    >
       <h1>Login page</h1>
       <PrimaryButton content="Iniciar sesión"></PrimaryButton>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const Dotenv = require('dotenv-webpack');
-
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "build"),
     filename: "bundle[fullhash].js",
-    publicPath: '/',
+    publicPath: "/",
   },
   mode: "development",
   devServer: {
+    https: true,
     port: 3000,
     open: true,
     compress: true,
     writeToDisk: true,
     historyApiFallback: true,
+    contentBase: path.join(__dirname, "assets"),
   },
   devtool: "eval-source-map",
   module: {
@@ -42,6 +43,24 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: "assets",
+              name(resourcePath, resourceQuery) {
+                if (process.env.NODE_ENV === 'development') {
+                  return '[path][name].[ext]';
+                }
+    
+                return '[contenthash].[ext]';
+              },
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -49,6 +68,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
-    new Dotenv()
+    new Dotenv(),
   ],
 };
