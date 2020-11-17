648cc4f584e7011c441d85aa2bc56f26b664bfc4
AlanGlezH <47785262+AlanGlezH@users.noreply.github.com>
2020-11-17 11:50:42 -0600

    Component LogoWorktion added

---
+++ src/components/LogoWorktion/LogoWorktion.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logoWorktion from "../../images/logoWorktion.png";
+import styled from "styled-components";
+
+const LogoImg = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const LogoWorktion = () => {
+  return <LogoImg src={logoWorktion} alt="Logo worktion"></LogoImg>;
+};
+
+export default LogoWorktion;
