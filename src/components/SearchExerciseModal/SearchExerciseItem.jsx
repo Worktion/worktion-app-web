502b915dd44bbc80a7f6f15269fcd49f915d2e5c
Victor Niño <ninomtz.victor@gmail.com>
2020-12-11 01:05:16 -0600

    Component to search exercises

---
+++ src/components/SearchExerciseModal/SearchExerciseItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../ExerciseItem/exerciseItemStyles.scss";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+
+const SearchExerciseItem = ({ exercise, handleSelectExercise }) => {
+  return (
+    <div className="search-container" onDoubleClick={() => handleSelectExercise(exercise)}>
+      <div className="exercise-img-container">
+        <img
+          src={exercise.images[0].image}
+          alt="Imagen de ejercicio"
+          style={{ width: "50px", height: "50px" }}
+          title={exercise.name}
+        ></img>
+
+        <div
+          style={{
+            marginLeft: "15px",
+            wordWrap: "break-word",
+          }}
+        >
+          {exercise.name.length <= 25
+            ? exercise.name
+            : exercise.name.substr(0, 25) + "..."}
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default SearchExerciseItem;
